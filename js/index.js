393555f6ea5d6dd85f3f4466a019efd058609cf2
TommyIQ <bmp110676@gmail.com>
2020-05-19 17:35:39 +0300

    lection is made

--- js/index.js
+++ js/index.js
@@ -1,31 +1,3 @@
-const toggleMenu = (triggerSel, menuSel, menuElemSel, additionalMenuClass, additionalTriggerClass, additionalTriggerElemClass) => {
-
-  function setClasses() {
-    menu.classList.toggle(additionalMenuClass);
-    trigger.classList.toggle(additionalTriggerClass);
-
-    const triggerElements = trigger.children;
-    
-    for(let i = 0; i < triggerElements.length; i++) {
-      triggerElements[i].classList.toggle(additionalTriggerElemClass);
-    }
-  }
-
-  const trigger = document.querySelector(triggerSel);
-  const menu = document.querySelector(menuSel);
-
-  trigger.addEventListener('click', (e) => {
-    setClasses();
-  });
-
-  menu.addEventListener('click', (e) => {
-    if(!e.target.closest(menuElemSel)) return;
-
-    setClasses();
-  });
-};
-
-
 const startSlider = (sliderSel) => {
   new Glide(sliderSel, {
     type: 'slider',
@@ -40,4 +12,3 @@ const startSlider = (sliderSel) => {
 
 
 startSlider('.glide');
-toggleMenu('.header__burger', '.header__menu', '.header__menu-item', 'header__menu-active', 'header__burger-active', 'header__burger-elem-active');
